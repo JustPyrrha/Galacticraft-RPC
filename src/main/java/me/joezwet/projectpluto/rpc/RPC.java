48b940e752a74b4b9d022c0b7fc2abb423674a14
Joe van der Zwet <me@joezwet.me>
2019-02-02 07:29:46 +1300

    Initial Commit

---
+++ src/main/java/me/joezwet/projectpluto/rpc/RPC.java
@@ -0,0 +1,8 @@
+package me.joezwet.projectpluto.rpc;
+
+import me.joezwet.projectpluto.rpc.util.Strings;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Strings.MODID, clientSideOnly = true, name = Strings.NAME, version = Strings.VERSION, updateJSON = Strings.UPDATEJSON)
+public class RPC {
+}
---
+++ src/main/java/me/joezwet/projectpluto/rpc/util/Strings.java
@@ -0,0 +1,9 @@
+package me.joezwet.projectpluto.rpc.util;
+
+public class Strings {
+
+    public static final String MODID = "rpc";
+    public static final String NAME = "Project Pluto Official Discord Rich Presence Link";
+    public static final String VERSION = "1.0";
+    public static final String UPDATEJSON = "";
+}
